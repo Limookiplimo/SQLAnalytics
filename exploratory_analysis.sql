002890f894d9a9ad4d8ba1dc4c2109cfbc4e2985
Limoo Kiplimo <limoodrice@gmail.com>
2023-05-09 19:30:53 +0300

    tune sql scripts

--- exploratory_analysis.sql
+++ exploratory_analysis.sql
@@ -1,3 +1,4 @@
+-- ====================================== ENTITIES AND ATTRIBUTES =========================================
 -- accounts
 select * from accounts a;
 -- billings
@@ -14,7 +15,7 @@ select * from "groups" g;
 select * from products p;
 
 
--- ===================================== EXPLORATORY ANALYSIS ============================================
+-- ===================================== EXPLORATORY ANALYSIS ==============================================
 -- Neg amounts
 select 
     id,
@@ -45,6 +46,7 @@ from receipts r;
 select * 
 from receipts r 
 where DATE_PART('year', r.effective_when::date) = 1900;
+
 
 -- Currency for transactions in the year '1900'
 select 
@@ -203,92 +205,13 @@ ORDER BY
   month_num;
 
 
- -- Monthly disbursement, collection and amount due 
- 
-with dis as (
-    select
-        disbursed_month,
-        month_num,
-        disbursed_year,
-        SUM(price_unlock) AS monthly_disbursement
-    from (
-        select
-            a.id as account_id,
-            DATE_PART('month', b.created_when::date) as month_num,
-            to_char(b.created_when ::date, 'Month') as disbursed_month,
-            DATE_PART('year', b.created_when ::date) as disbursed_year,
-            b.price_unlock as price_unlock,
-            substring(a.nominal_term from '"days":[ ]*([0-9]+)[ ]*')::integer as loan_duration_days,
-            round(substring(a.nominal_term from '"days":[ ]*([0-9]+)[ ]*')::numeric /
-            DATE_PART('day', DATE_TRUNC('month', a.registration_date::date + INTERVAL '1 MONTH') - DATE_TRUNC('month', a.registration_date::date))::numeric,1) as loan_duration_month
-        from
-            billings b
-            inner join accounts a on a.billing_id = b.id
-    ) as am_due
-    group by
-        disbursed_year,
-        disbursed_month,
-        month_num
-    order by
-        disbursed_year,
-        month_num
-), coll as (
-    select
-        collection_year,
-        collection_month,
-        SUM(collected_payment) as monthly_collection
-    from (
-        select
-            a.id as account_id,
-            DATE_PART('month', r.effective_when::date) as month_num,
-            to_char(r.effective_when::date, 'Month') as collection_month,
-            DATE_PART('year', r.effective_when::date) as collection_year,
-            case
-                when r.currency = 'USD' and DATE_PART('year', r.effective_when::date) = 2022 then r.amount * 100
-                when r.currency = 'USD' and DATE_PART('year', r.effective_when::date) = 2023 then r.amount * 110
-                else r.amount
-            end as collected_payment
-        from
-            payments p
-            inner join accounts a on p.account_id = a.id
-            inner join receipts r on p.receipt_id = r.id
-        where
-            r.amount >= 0
-            and DATE_PART('year', r.effective_when::date) in (2022, 2023)
-    ) as coll
-    group by
-        collection_year,
-        collection_month,
-        month_num
-    order by
-        collection_year,
-        month_num
-)
-select
-    dis.disbursed_month,
-    dis.disbursed_year,
-    dis.monthly_disbursement,
-    coll.monthly_collection,
-    case
-        when coll.monthly_collection is null then dis.monthly_disbursement
-        when coll.monthly_collection > dis.monthly_disbursement then dis.monthly_disbursement
-        else dis.monthly_disbursement - coll.monthly_collection
-    end as amount_due
-from
-    coll
-    right join dis on coll.collection_year = dis.disbursed_year and coll.collection_month = dis.disbursed_month
-order by
-    dis.disbursed_year,
-    dis.month_num;
-   
- -- ==================================================== OCR ======================================================  
--- Overall collection rate
+ -- ================================================ OVERALL COLLECTION RATE ======================================================
 
 -- Principal balance
 select price_unlock - price_upfront as principal_rem_balance
 from billings b;
 
---Interest amount - assumptions(daily rate is 5%)
+--Interest amount - (Assumption: daily rate 5% of 5% interest)
 select round(rem_balance * (0.05/365) * down_payment_period,1) as interest_on_downpayment
 from(
 	select price_unlock - price_upfront as rem_balance,
@@ -297,29 +220,25 @@ from(
 ) as sub;
 
 -- Remaning balance after downpayment
-select round(price_unlock - price_upfront + down_payment_interest,0) as amount_due
+select round(rem_balance + down_payment_interest,0) as amount_due
 from(
 select
-	price_unlock,
-	price_upfront,
 	price_unlock - price_upfront as rem_balance,
 	substring(down_payment_period from '"days":[ ]*([0-9]+)[ ]*')::integer as down_payment_period,
 	(price_unlock - price_upfront)  * (0.05/365) * (substring(down_payment_period from '"days":[ ]*([0-9]+)[ ]*')::integer) as down_payment_interest
 from billings b2 
 ) as sub;
 
--- Installments - assumption(monthly installments)
+-- Number of Installments - (Assumption:monthly payment of installments)
 select
-	ceiling ((substring(a.nominal_term from '"days":[ ]*([0-9]+)[ ]*')::integer)/30) as num_installments
+	ceiling ((substring(a.nominal_term from '"days":[ ]*([0-9]+)[ ]*')::integer) / 30) as num_installments
 from accounts a;
 
--- Intallment amount(amount_due)
-select round((loan_amount - down_payment_amount + down_payment_interest) / (ceiling (loan_duration /30))) as intallment_amount
+-- Next installment amount
+select round(amount_due / (ceiling (loan_duration /30))) as first_installment_amount
 from(
 	select
-		loan_amount,
-		down_payment_amount,
-		down_payment_period,
+		round(loan_amount - down_payment_amount + (rem_balance * (0.05/365) * down_payment_period),0) as amount_due,
 		loan_duration,
 		(rem_balance * (0.05/365) * down_payment_period) as down_payment_interest
 	from(
@@ -334,23 +253,149 @@ from(
 		) as sub1
 	) as sub2;
 
--- First due_payment after downpaymnet is paid
+-- Overall Collection Rate
+with m_coll as (
+	select
+	  	collection_year,
+	  	collection_month,
+	  	SUM(collected_payment) AS monthly_collection
+	from
+	  (
+	    select
+	      DATE_PART('month', r.effective_when::date) as month_num,
+	      to_char(r.effective_when::date, 'Month') as collection_month,
+	      DATE_PART('year', r.effective_when::date) AS collection_year,
+	      case
+	        when r.currency = 'USD' and DATE_PART('year', r.effective_when::date) = 2022 then r.amount * 100
+	        when r.currency = 'USD' and DATE_PART('year', r.effective_when::date) = 2023 then r.amount * 110
+	        else r.amount
+	      end as collected_payment
+	    from payments p
+	    inner join receipts r on p.receipt_id = r.id
+	    where
+	      r.amount >= 0
+	      and DATE_PART('year', r.effective_when::date) in (2022, 2023)
+	      and r.id not in (select
+	      						p.receipt_id
+							from payments p
+							inner join receipts r on p.receipt_id  = r.id
+							where r.amount not in (select b.price_upfront from billings b))
+		) as sub
+	group by collection_year,collection_month,month_num
+	 order by collection_year,month_num
+),
+am_due as(
+	select
+		month_num,
+		next_payment_month,
+		next_payment_year,
+		sum(round(amount_due / (ceiling (loan_duration /30)))) as monthly_due
+	from(
+		select
+			next_payment_month,
+			month_num,
+			next_payment_year,
+			round(loan_amount - down_payment_amount + (rem_balance * (0.05/365) * down_payment_period),0) as amount_due,
+			loan_duration,			(rem_balance * (0.05/365) * down_payment_period) as down_payment_interest
+		from(
+			select
+				DATE_PART('month', b.created_when::date) AS month_num,
+				to_char(b.created_when ::date, 'Month') AS next_payment_month,
+				DATE_PART('year', b.created_when ::date) AS next_payment_year,
+				price_unlock as loan_amount,
+				substring(a.nominal_term from '"days":[ ]*([0-9]+)[ ]*')::integer as loan_duration,
+				price_upfront as down_payment_amount,
+				price_unlock - price_upfront as rem_balance,
+				substring(down_payment_period from '"days":[ ]*([0-9]+)[ ]*')::integer as down_payment_period
+			from accounts a
+			inner join billings b on a.billing_id = b.id
+			) as sub1
+		) as sub2
+	group by next_payment_year,next_payment_month,month_num
+	ORDER by next_payment_year,month_num
+),
+aggs as (
+	select
+		am_due.month_num,
+	    m_coll.collection_year as year,
+	  	m_coll.collection_month as month,
+	  	m_coll.monthly_collection as monthly_collection,
+	    am_due.monthly_due
+	from m_coll
+	right join am_due on m_coll.collection_year = am_due.next_payment_year and m_coll.collection_month = am_due.next_payment_month
+	order by am_due.next_payment_year,am_due.next_payment_month
+)
+select
+	aggs.year,
+	aggs.month,
+	round(aggs.monthly_collection/monthly_due) as collection_rate
+from aggs
+group by monthly_collection,monthly_due,year,month,month_num 
+order by year,month_num;
+
+
+-- ================================================== FIRST PAYMENT ON TIME =================================================
+-- First installment payment date
 select 
 	created_when::date as down_payment_date,
 	(NULLIF(created_when, '')::date + INTERVAL '30 DAY')::date as first_payment_date
 from billings;
 
--- ==================================================== FPOT ===========================================================
- -- First payment on time
+-- First installment amount
+select round(amount_due / (ceiling (loan_duration /30))) as first_installment_amount
+from(
+	select
+		round(loan_amount - down_payment_amount + (rem_balance * (0.05/365) * down_payment_period),0) as amount_due,
+		loan_duration,
+		(rem_balance * (0.05/365) * down_payment_period) as down_payment_interest
+	from(
+		select
+			price_unlock as loan_amount,
+			substring(a.nominal_term from '"days":[ ]*([0-9]+)[ ]*')::integer as loan_duration,
+			price_upfront as down_payment_amount,
+			price_unlock - price_upfront as rem_balance,
+			substring(down_payment_period from '"days":[ ]*([0-9]+)[ ]*')::integer as down_payment_period
+		from accounts a
+		inner join billings b on a.billing_id = b.id 
+		) as sub1
+	) as sub2;
 
--- First due_payment after downpaymnet is paid
-select 
-	created_when::date as down_payment_date,
-	(NULLIF(created_when, '')::date + INTERVAL '30 DAY')::date as first_payment_date
-from billings;
 
-
-
+-- Total amount of payments collected from each merchant's account
+select
+	first_installment_amount,
+	case 
+		when actual_payment_date <= first_payment_date THEN 'On time' ELSE 'Late'
+	end as first_payment_status 
+from(
+	select 
+	round(amount_due / (ceiling (loan_duration /30))) as first_installment_amount,
+	first_payment_date,
+	actual_payment_date
+from(
+	select
+		round(loan_amount - down_payment_amount + (rem_balance * (0.05/365) * down_payment_period),0) as amount_due,
+		loan_duration,
+		(rem_balance * (0.05/365) * down_payment_period) as down_payment_interest,
+		actual_payment_date,
+		first_payment_date
+	from(
+		select
+			NULLIF(r.effective_when, '')::date as actual_payment_date,
+			price_unlock as loan_amount,
+			substring(a.nominal_term from '"days":[ ]*([0-9]+)[ ]*')::integer as loan_duration,
+			price_upfront as down_payment_amount,
+			price_unlock - price_upfront as rem_balance,
+			substring(down_payment_period from '"days":[ ]*([0-9]+)[ ]*')::integer as down_payment_period,
+			(NULLIF(b.created_when, '')::date + INTERVAL '30 DAY')::date as first_payment_date
+		from payments p
+		inner join receipts r on p.receipt_id = r.id
+		inner join accounts a on p.account_id = a.id 
+		inner join billings b on a.billing_id = b.id
+		where r.amount <> b.price_upfront
+		) as sub1
+	) as sub2
+) as sub3;
 
 -- ===================================== THE END ==================================================
 
